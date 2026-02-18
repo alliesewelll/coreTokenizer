5342460351c86c3235e98fb0223758064fc1692d
Allie <alliesewell@gmail.com>
2026-02-18 11:23:50 -0500

    Changes

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,7 @@
 import sys
 
 RESERVED_KEYWORDS = {
-    "program" : 1,
+    "program" : 1, 
     "begin" : 2,
     "end" : 3,
     "int" : 4,
@@ -35,3 +35,39 @@ SYMBOLS = {
     "<=" : 29,
     ">=" : 30
 }
+
+TOK_INT = 31
+TOK_ID = 32
+TOK_EOF = 33
+TOK_ERR = 34
+
+class Tokenizer:
+    def __init__(self, filename):
+        self.reader = open(filename, 'r', encoding='utf-8')
+        self.tokens: list[tuple[int, str]] = []
+        self.current_token_index = 0
+        self.eof = False
+        self.tokenizeLine()
+
+    def getToken(self) -> int:
+        return self.tokens[self.current_token_index][0]
+    
+    def skipToken(self):
+        t = self.getToken()
+        if t in (TOK_EOF, TOK_ERR):
+            return
+        self.current_token_index += 1
+        if self.current_token_index >= len(self.tokens):
+            self.tokenizeLine()
+            
+    def intVal(self) -> int:
+        if self.getToken() != TOK_INT:
+            raise Exception("Current token is not an integer")
+        return int(self.tokens[self.current_token_index][1])  
+    
+    def idName(self) -> str:
+        if self.getToken() != TOK_ID:
+            raise Exception("Current token is not an identifier")
+        return self.tokens[self.current_token_index][1]
+    
+        
